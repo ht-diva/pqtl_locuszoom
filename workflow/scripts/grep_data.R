2dc9adb81aabe8a72e69d6803868dd35aab8d4c3
dariushghasemi <ghasemi.dariush@yahoo.com>
2025-06-26 18:09:37 +0200

    R script to grep target conditional data from RDS

---
+++ workflow/scripts/grep_data.R
@@ -0,0 +1,25 @@
+#!/usr/bin/Rscript
+
+suppressMessages(library(data.table))
+
+
+# recall inputs from snakemake
+path_rds <- snakemake@input[["cond_rds"]]
+cojo_snp <- snakemake@params[["cojo_snp"]]
+ofile <- snakemake@output[["ofile"]]
+
+"/scratch/dariush.ghasemi/projects/pqtl_pipeline_finemap/results/meta_correct_sdy/cojo/seq.8528.74/conditional_data_17_7063650_7173279.rds"
+cojo_snp <- "17:7063667:C:T"
+
+# read conditional RDS file
+df_rds <- readRDS(path_rds)
+
+# trow number for the input COJO  variant
+k <- which(df_rds$ind.snps$SNP == cojo_snp)
+
+# store the right data from a list of conditional datasets
+df_cond <- df_rds$results[[1]]
+
+
+# save reformatted conditional data
+data.table::fwrite(df_rds, file = , quote = F, row.names = F, sep = "\t")
